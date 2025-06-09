89145c7ccd5b657c9203db8c2f84fba28e16c090
Bohdan Shevchuk <shevdan007@gmail.com>
2025-06-09 17:16:52 +0300

    upd code to logging; env vars in consumer

--- microservices/consumer/app.py
+++ microservices/consumer/app.py
@@ -1,14 +1,27 @@
+import os
+import logging
 import faust
 import tldextract
 from collections import Counter
 
+SERVICE_NAME = os.environ.get('SERVICE_NAME', 'Unknown Service')
+KAFKA_BROKER = os.environ.get('KAFKA_BROKER', 'localhost:9092')
+TOPIC_NAME = os.environ.get('TOPIC_NAME', 'history-topic')
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+
 app = faust.App(
     'history-stream-app',
-    broker='kafka://redpanda:29092',
+    broker=f'kafka://{KAFKA_BROKER}',
     value_serializer='json'
 )
 
-history_topic = app.topic('history-topic')
+history_topic = app.topic(TOPIC_NAME)
 
 domain_counter = Counter()
 
@@ -22,10 +35,10 @@ async def process(history_events):
             domain_counter[root_domain] += 1
 
         if sum(domain_counter.values()) % 10 == 0:
-            print("\n\nTop 5 root domains:")
+            logger.info("\n\nTop 5 root domains:")
             for domain, count in domain_counter.most_common(5):
-                print(f"{domain} - {count} visits")
-            print("==="*20)
+                logger.info(f"{domain} - {count} visits")
+            logger.info("==="*20)
 
 if __name__ == '__main__':
     app.main()
